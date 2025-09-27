2f4773e649577d198a49bcbdd1ac661d895720bb
Sazedur <sazedur11ab@gmail.com>
2025-09-27 20:36:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h4 className='text-6xl text-blue-300 text-center font-bold mt-4'>Vite + React</h4>
+     
+    </>
+  )
+}
+
+export default App
